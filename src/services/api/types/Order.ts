e9cb3f7457be62c16ac26433a8bf6f647b267cf0
Zé Lucas Chociai <joselucaschociai@gmail.com>
2023-12-07 11:24:15 -0300

    fix

--- src/services/api/types/Order.ts
+++ src/services/api/types/Order.ts
@@ -10,7 +10,7 @@ export enum OrderStatus {
   CanceledByPartner = 'canceled_by_partner',
   CanceledByCustomer = 'canceled_by_customer',
   AcceptedByPartner = 'accepted_by_partner',
-  RefusedByPartner = 'refused_by_Partner',
+  RefusedByPartner = 'refused_by_partner',
   AwaitingExecution = 'awaiting_execution',
   InDelivery = 'in_delivery',
   Settled = 'settled'
